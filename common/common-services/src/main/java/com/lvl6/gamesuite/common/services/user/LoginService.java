f7802281fbdf46b19e1f588fd2323be6f3679be5
Arturo Wu Zhou <aweezy90@gmail.com>
2013-05-02 20:38:58 -0700

    update user's last login time; changed method that checks user's password

--- common/common-services/src/main/java/com/lvl6/gamesuite/common/services/user/LoginService.java
+++ common/common-services/src/main/java/com/lvl6/gamesuite/common/services/user/LoginService.java
@@ -1,13 +1,33 @@
 package com.lvl6.gamesuite.common.services.user;
 
+import org.joda.time.DateTime;
+
+import com.lvl6.gamesuite.common.dao.UserDao;
+import com.lvl6.gamesuite.common.po.AuthorizedDevice;
 import com.lvl6.gamesuite.common.po.User;
+import com.lvl6.gamesuite.common.services.authorizeddevice.AuthorizedDeviceService;
 import com.lvl6.gamesuite.user.utils.PasswordUtil;
 
 public interface LoginService {
 
+  /**
+   * Return the login token (via an AuthorizedDevice object) so the user doesn't have to
+   * keep providing personal information.
+   */
+  public abstract AuthorizedDevice updateUserLastLogin(User inDb, DateTime now, String udid, String deviceId);
+  
   public abstract boolean validCredentials(User inDb, String nameStrangersSee, String email, String password);
 
   public abstract PasswordUtil getPasswordUtil();
 
   public abstract void setPasswordUtil(PasswordUtil passwordUtil);
+  
+  public abstract UserDao getUserDao();
+
+  public abstract void setUserDao(UserDao userDao);
+  
+  public AuthorizedDeviceService getAuthorizedDeviceService();
+  
+  public void setAuthorizedDeviceService(AuthorizedDeviceService authorizedDeviceService);
+  
 }
--- common/common-services/src/main/java/com/lvl6/gamesuite/common/services/user/LoginServiceImpl.java
+++ common/common-services/src/main/java/com/lvl6/gamesuite/common/services/user/LoginServiceImpl.java
@@ -2,9 +2,13 @@ package com.lvl6.gamesuite.common.services.user;
 
 import java.util.Date;
 
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.lvl6.gamesuite.common.dao.UserDao;
+import com.lvl6.gamesuite.common.po.AuthorizedDevice;
 import com.lvl6.gamesuite.common.po.User;
+import com.lvl6.gamesuite.common.services.authorizeddevice.AuthorizedDeviceService;
 import com.lvl6.gamesuite.user.utils.PasswordUtil;
 
 public class LoginServiceImpl implements LoginService {
@@ -13,15 +17,45 @@ public class LoginServiceImpl implements LoginService {
   @Autowired
   protected PasswordUtil passwordUtil;
 
+  @Autowired
+  protected UserDao userDao; 
+  
+  @Autowired
+  protected AuthorizedDeviceService authorizedDeviceService;
+  
+
+  @Override
+  public AuthorizedDevice updateUserLastLogin(User inDb, DateTime now, String udid, String deviceId) {
+    inDb.setLastLogin(now.toDate());
+    userDao.save(inDb);
+    
+    //return login token inside of the authorized device
+    String userId = inDb.getId();
+    AuthorizedDevice ad = getAuthorizedDeviceService().checkForExistingAuthorizedDevice(userId, udid);
+    if (null == ad) {
+      ad = getAuthorizedDeviceService().registerNewAuthorizedDevice(userId, udid, deviceId);
+    }
+    getAuthorizedDeviceService().updateExpirationForAuthorizedDevice(ad);
+    return ad;
+  }
+  
   @Override
   public boolean validCredentials(User inDb, String nameStrangersSee, String email, String password) {
-    String encodedPassword = passwordUtil.encodePassword(password, getSalt(inDb.getSignupDate()));
-    if(encodedPassword.equals(inDb.getPassword()) && inDb.getNameStrangersSee().equals(nameStrangersSee) 
-        && inDb.getEmail().equals(email)) {
-      return true; 
-    }else {
-      return false;
+    boolean success = false;
+    try {
+      byte[] salt = getSalt(inDb.getSignupDate());
+      String encodedPassword = passwordUtil.encodePassword(password, salt);
+      boolean isPasswordValid = passwordUtil.isPasswordValid(encodedPassword, password, salt);
+      if(isPasswordValid && inDb.getNameStrangersSee().equals(nameStrangersSee) 
+          && inDb.getEmail().equals(email)) {
+        success = true; 
+      }else {
+        success = false;
+      }
+    } catch (Exception e) {
+      success = false;
     }
+    return success;
   }
 
   protected byte[] getSalt(Date signupDate) {
@@ -38,5 +72,26 @@ public class LoginServiceImpl implements LoginService {
   @Override
   public void setPasswordUtil(PasswordUtil passwordUtil) {
     this.passwordUtil = passwordUtil;
+  }
+
+  @Override
+  public UserDao getUserDao() {
+    return userDao;
+  }
+
+  @Override
+  public void setUserDao(UserDao userDao) {
+    this.userDao = userDao;
+  }
+  
+  @Override
+  public AuthorizedDeviceService getAuthorizedDeviceService() {
+    return authorizedDeviceService;
+  }
+
+  @Override
+  public void setAuthorizedDeviceService(
+      AuthorizedDeviceService authorizedDeviceService) {
+    this.authorizedDeviceService = authorizedDeviceService;
   }
 }
