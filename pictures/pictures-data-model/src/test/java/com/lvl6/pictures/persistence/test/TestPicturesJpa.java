83f8a655605390a7da496d1a333cd730b0b48f30
Arturo Wu Zhou <aweezy90@gmail.com>
2013-05-05 00:44:33 -0700

    making sure multiple choice question and answers show up in db

--- pictures/pictures-data-model/src/test/java/com/lvl6/pictures/persistence/test/TestPicturesJpa.java
+++ pictures/pictures-data-model/src/test/java/com/lvl6/pictures/persistence/test/TestPicturesJpa.java
@@ -9,11 +9,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lvl6.pictures.dao.MultipleChoiceQuestionDao;
 import com.lvl6.pictures.dao.PictureQuestionWithTextAnswerDao;
+import com.lvl6.pictures.po.AnswerType;
+import com.lvl6.pictures.po.MultipleChoiceAnswer;
+import com.lvl6.pictures.po.MultipleChoiceQuestion;
 import com.lvl6.pictures.po.PicturesQuestionWithTextAnswer;
 
 
@@ -29,8 +34,10 @@ public class TestPicturesJpa {
 	protected PictureQuestionWithTextAnswerDao pictureDao;
 	
 	@Resource 
-	protected MultipleChoiceQuestionDao multipleChoiceDao;
+	protected MultipleChoiceQuestionDao multipleChoiceQuestionDao;
 	
+	@Transactional
+  @Rollback(true)
 	@Test
 	public void testSetup() {
 		log.info("Testing Pictures JPA");
@@ -46,8 +53,90 @@ public class TestPicturesJpa {
 		q = getPictureDao().save(q);
 		log.info("Created Picture question: {}", q.getId());
 	}
-
-
+	
+	@Transactional
+  @Rollback(false)
+	@Test
+	public void testMultipleChoice() {
+	  MultipleChoiceQuestion mcq = new MultipleChoiceQuestion();
+	  String question = "Which drink is made from the leaves of a plant?";
+	  mcq.setQuestion(question);
+	  mcq.setCreatedBy("JUnit");
+	  
+	  MultipleChoiceAnswer mcaA = new MultipleChoiceAnswer();
+    String answerA = "milk";
+    mcaA.setAnswer(answerA);
+    mcaA.setAnswerType(AnswerType.TEXT);
+    mcaA.setCorrect(false);
+    
+    MultipleChoiceAnswer mcaB = new MultipleChoiceAnswer();
+    String answerB = "orange juice";
+    mcaB.setAnswer(answerB);
+    mcaB.setAnswerType(AnswerType.TEXT);
+    mcaB.setCorrect(false);
+    
+    MultipleChoiceAnswer mcaC = new MultipleChoiceAnswer();
+    String answerC = "coffee";
+    mcaC.setAnswer(answerC);
+    mcaC.setAnswerType(AnswerType.TEXT);
+    mcaC.setCorrect(false);
+    
+    MultipleChoiceAnswer mcaD = new MultipleChoiceAnswer();
+    String answerD = "tea";
+    mcaD.setAnswer(answerD);
+    mcaD.setAnswerType(AnswerType.TEXT);
+    mcaD.setCorrect(true);
+    
+    
+    Set<MultipleChoiceAnswer> answers = new HashSet<MultipleChoiceAnswer>();
+    answers.add(mcaA);
+    answers.add(mcaB);
+    answers.add(mcaC);
+    answers.add(mcaD);
+	  mcq.setAnswers(answers);
+	  
+	  
+	  getMultipleChoiceDao().save(mcq);
+	}
+	
+//	@Transactional
+//  @Rollback(true)
+//  @Test
+//  public void testQuestionsAnswered() {
+//	  
+//	  int roundNumber = 1;
+//	  int questionNumber = 1;
+//	  int questionNumber2 = 2;
+//	  Date answeredDate = new Date();
+//	  String answeredByUser = "me";
+//	  
+//    QuestionAnswered qa = new QuestionAnswered();
+//    qa.setRoundNumber(roundNumber);
+//    qa.setQuestionNumber(questionNumber);
+//    qa.setAnsweredDate(answeredDate);
+//    
+//    QuestionAnswered qa2 = new QuestionAnswered();
+//    qa2.setRoundNumber(roundNumber);
+//    qa2.setQuestionNumber(questionNumber2);
+//    qa2.setAnsweredDate(answeredDate);
+//    qa2.setAnsweredByUser(answeredByUser);
+//  }
+//
+//	@Transactional
+//  @Rollback(true)
+//  @Test
+//  public void testRoundHistory() {
+//    
+//  }
+//	
+//	@Transactional
+//  @Rollback(true)
+//	@Test
+//	public void testGameHistory() {
+//	  
+//	}
+	
+	
 
 	public PictureQuestionWithTextAnswerDao getPictureDao() {
 		return pictureDao;
@@ -62,18 +151,14 @@ public class TestPicturesJpa {
 
 
 	public MultipleChoiceQuestionDao getMultipleChoiceDao() {
-		return multipleChoiceDao;
+		return multipleChoiceQuestionDao;
 	}
 
 
 
 	public void setMultipleChoiceDao(MultipleChoiceQuestionDao multipleChoiceDao) {
-		this.multipleChoiceDao = multipleChoiceDao;
+		this.multipleChoiceQuestionDao = multipleChoiceDao;
 	}
-
-
-
-
 
 
 }
