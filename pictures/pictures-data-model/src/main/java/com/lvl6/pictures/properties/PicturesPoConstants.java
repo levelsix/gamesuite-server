3a37abbad1fa433a0fe2852b99a5f564c4d0f0e6
Arturo Wu Zhou <aweezy90@gmail.com>
2013-05-07 15:34:02 -0700

    constants

---
+++ pictures/pictures-data-model/src/main/java/com/lvl6/pictures/properties/PicturesPoConstants.java
@@ -0,0 +1,15 @@
+package com.lvl6.pictures.properties;
+
+public class PicturesPoConstants {
+  
+  //user
+  public static int USER__DEFAULT_INITIAL_TOKENS = 10;
+  public static int USER__DEFAULT_MAX_TOKENS = 20;
+  public static int USER__DEFAULT_INITIAL_RUBIES = 20;
+  
+  //game history
+  public static int GAME_HISTORY__DEFAULT_NUM_DISPLAYED_GAMES = 5;  //might not be used
+  public static int GAME_HISTORY__DEFAULT_COMPLETED_GAMES_MIN_DAYS_DISPLAYED= 2; //might not be used
+  
+  
+}
