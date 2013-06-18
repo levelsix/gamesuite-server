17d4fc0b0b6e85c1cb2a214132c39a2011aeaf1a
Arturo Wu Zhou <aweezy90@gmail.com>
2013-06-18 00:08:48 -0700

    got rid of logging, changed indentation from 2 to 4

--- pictures/pictures-services/src/main/java/com/lvl6/pictures/services/utils/RandomNumberUtils.java
+++ pictures/pictures-services/src/main/java/com/lvl6/pictures/services/utils/RandomNumberUtils.java
@@ -10,27 +10,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class RandomNumberUtils {
 
-  protected Random rand = new Random();
+    protected Random rand = new Random();
 
-  public Collection<Integer> generateNRandomIntsBelowInt(
-      int upperBound, int n) {
-    Set<Integer> randomInts = new HashSet<Integer>();
-    
-    //loop until we have number of ints = amount
-    while (n > randomInts.size()) {
-      int randInt = rand.nextInt(upperBound);
-      randomInts.add(randInt);
+    public Collection<Integer> generateNRandomIntsBelowInt(
+	    int upperBound, int n) {
+	Set<Integer> randomInts = new HashSet<Integer>();
+
+	//loop until we have number of ints = amount
+	while (n > randomInts.size()) {
+	    int randInt = rand.nextInt(upperBound);
+	    randomInts.add(randInt);
+	}
+	return randomInts;
     }
-    return randomInts;
-  }
-  
-  
-  public Random getRand() {
-    return rand;
-  }
 
-  public void setRand(Random rand) {
-    this.rand = rand;
-  }
-  
+
+    public Random getRand() {
+	return rand;
+    }
+
+    public void setRand(Random rand) {
+	this.rand = rand;
+    }
+
 }
