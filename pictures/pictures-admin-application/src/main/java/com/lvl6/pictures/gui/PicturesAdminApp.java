2cbec3096234c51d7409018b723f811de2986a7c
bluestix <endgame2013@gmail.com>
2013-06-18 01:20:17 -0700

    Adding admin gui project

---
+++ pictures/pictures-admin-application/src/main/java/com/lvl6/pictures/gui/PicturesAdminApp.java
@@ -0,0 +1,34 @@
+package com.lvl6.pictures.gui;
+
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+
+public class PicturesAdminApp implements Application {
+
+	public void resume() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean shutdown(boolean arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void startup(Display arg0, Map<String, String> arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void suspend() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+	    DesktopApplicationContext.main(PicturesAdminApp.class, args);
+	}
+
+}
