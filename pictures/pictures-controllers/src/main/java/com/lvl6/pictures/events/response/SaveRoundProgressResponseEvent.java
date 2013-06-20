da70883f7d9556ecc700c891337a1c390d5fb048
Arturo Wu Zhou <aweezy90@gmail.com>
2013-06-20 16:15:21 -0700

    java files for save round progress request and response proto

---
+++ pictures/pictures-controllers/src/main/java/com/lvl6/pictures/events/response/SaveRoundProgressResponseEvent.java
@@ -0,0 +1,30 @@
+package com.lvl6.pictures.events.response;
+import java.nio.ByteBuffer;
+
+import com.google.protobuf.ByteString;
+import com.lvl6.gamesuite.common.events.NormalResponseEvent;
+import com.lvl6.pictures.eventprotos.SaveRoundProgressEventProto.SaveRoundProgressResponseProto;
+import com.lvl6.pictures.noneventprotos.PicturesEventProtocolProto.PicturesEventProtocolResponse;
+
+
+public class SaveRoundProgressResponseEvent extends NormalResponseEvent {
+
+  private SaveRoundProgressResponseProto saveRoundProgressResponseProto;
+  
+  public SaveRoundProgressResponseEvent(String playerId) {
+    super(playerId);
+    eventType = PicturesEventProtocolResponse.S_SAVE_ROUND_PGROGRESS_EVENT_VALUE;
+  }
+  
+  @Override
+  public int write(ByteBuffer bb) {
+    ByteString b = saveRoundProgressResponseProto.toByteString();
+    b.copyTo(bb);
+    return b.size();
+  }
+  
+  public void setSaveRoundProgressResponseProto(SaveRoundProgressResponseProto saveRoundProgressResponseProto) {
+    this.saveRoundProgressResponseProto = saveRoundProgressResponseProto;
+  }
+  
+}
