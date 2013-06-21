a91646125c09f759034cac53e59f96e839de978d
bluestix <endgame2013@gmail.com>
2013-06-21 01:39:25 -0700

    Adding admin page

---
+++ pictures/pictures-webapp/src/main/java/com/lvl6/pictures/ui/admin/AdminPage.java
@@ -0,0 +1,20 @@
+package com.lvl6.pictures.ui.admin;
+
+
+
+public class AdminPage extends TemplatePage {
+
+	private static final long serialVersionUID = -1728365297134290240L;
+	
+
+	public AdminPage() {
+		super();
+
+	}
+	
+
+	
+	
+	
+
+}
--- pictures/pictures-webapp/src/main/java/com/lvl6/pictures/ui/admin/PicturesAdmin.java
+++ pictures/pictures-webapp/src/main/java/com/lvl6/pictures/ui/admin/PicturesAdmin.java
@@ -6,11 +6,21 @@ import org.apache.wicket.protocol.http.WebApplication;
 public class PicturesAdmin extends WebApplication {
 
 	
+
 	
 	
+	@Override
+	protected void init() {
+		super.init();
+		mountPage("/questions", QuestionEditorPage.class);
+		mountPage("/admin", AdminPage.class);
+	}
+
 	@Override
 	public Class<? extends Page> getHomePage() {
 		return MainPage.class;
 	}
+	
+	
 
 }
