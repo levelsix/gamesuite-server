9a47ec6d1ed98bc6aed72a56834090d9b2e06825
Arturo Wu Zhou <aweezy90@gmail.com>
2013-06-25 00:48:20 -0700

    comments, reformatting

--- common/common-data-model/src/main/java/com/lvl6/gamesuite/common/dao/UserDao.java
+++ common/common-data-model/src/main/java/com/lvl6/gamesuite/common/dao/UserDao.java
@@ -2,7 +2,6 @@ package com.lvl6.gamesuite.common.dao;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,21 +9,22 @@ import com.lvl6.gamesuite.common.po.User;
 
 public interface UserDao extends JpaRepository<User, String> {
 
-	public List<User> findByNameStrangersSee(String nameStrangersSee);
-	
-	public List<User> findByEmail(String email);
-	
-	public List<User> findByFacebookId(String facebookId);
-	
-	public List<User> findByFacebookIdIn(Collection<String> facebookId);
-	
-	public List<User> findByFacebookIdOrEmail(String facebookId, String email);
-	
-	public List<User> findByEmailOrNameStrangersSeeAndPasswordIsNotNull(String email, String nameStrangersSee);
-	
-	public User findById(String id);
-	
-	//test this
-	public Map<String, User> findByIdIn(Collection<String> idList);
-	
+    public List<User> findByNameStrangersSee(String nameStrangersSee);
+
+    public List<User> findByEmail(String email);
+
+    public List<User> findByFacebookId(String facebookId);
+
+    public List<User> findByFacebookIdIn(Collection<String> facebookId);
+
+    public List<User> findByFacebookIdOrEmail(String facebookId, String email);
+
+    public List<User> findByEmailOrNameStrangersSeeAndPasswordIsNotNull(String email, String nameStrangersSee);
+
+    public User findById(String id);
+
+    //test this (spring doesn't play well with Map<>
+    //public Map<String, User> findByIdIn(Collection<String> idList);
+    public List<User> findByIdIn(Collection<String> idList);
+
 }
