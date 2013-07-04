230355189d9f77e120a2d1f558f72ee9f3dddeb3
bluestix <endgame2013@gmail.com>
2013-07-03 21:14:20 -0700

    Testing a transactional change

--- pictures/pictures-controllers/src/main/java/com/lvl6/pictures/controller/LoginController.java
+++ pictures/pictures-controllers/src/main/java/com/lvl6/pictures/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lvl6.gamesuite.common.controller.EventController;
 import com.lvl6.gamesuite.common.eventprotos.ForceLogoutProto.ForceLogoutResponseProto;
@@ -86,6 +87,7 @@ public class LoginController extends EventController {
     }
 
     @Override
+    @Transactional
     protected void processRequestEvent(RequestEvent event) throws Exception {
 	LoginRequestProto reqProto = ((LoginRequestEvent) event).getLoginRequestProto();
 	//log.info("reqProto=" + reqProto);
