06d5f0e423adbe003c3ea3c84e3c0e27942c07d4
Arturo Wu Zhou <aweezy90@gmail.com>
2013-07-09 18:42:04 -0700

    indentation

--- common/common-controllers/src/main/java/com/lvl6/gamesuite/common/controller/utils/TimeUtils.java
+++ common/common-controllers/src/main/java/com/lvl6/gamesuite/common/controller/utils/TimeUtils.java
@@ -8,39 +8,39 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TimeUtils {
-  
-  public static int NUM_MINUTES_LEEWAY_FOR_CLIENT_TIME = 10;
-  
-  public boolean isSynchronizedWithServerTime(Date maybeNow) {
-    if (null == maybeNow) {
-      return false;
+
+    public static int NUM_MINUTES_LEEWAY_FOR_CLIENT_TIME = 10;
+
+    public boolean isSynchronizedWithServerTime(Date maybeNow) {
+	if (null == maybeNow) {
+	    return false;
+	}
+	DateTime possiblyNow = new DateTime(maybeNow); 
+	DateTime now = new DateTime();
+	Period interim = new Period(possiblyNow, now);
+
+	int minutesApart = interim.getMinutes();
+	if (minutesApart > NUM_MINUTES_LEEWAY_FOR_CLIENT_TIME) {
+	    //client time is unsynchronized with server time
+	    return false;
+	} else {
+	    return true;
+	}
     }
-    DateTime possiblyNow = new DateTime(maybeNow); 
-    DateTime now = new DateTime();
-    Period interim = new Period(possiblyNow, now);
-    
-    int minutesApart = interim.getMinutes();
-    if (minutesApart > NUM_MINUTES_LEEWAY_FOR_CLIENT_TIME) {
-      //client time is unsynchronized with server time
-      return false;
-    } else {
-      return true;
+
+    public int numMinutesDifference(Date d1, Date d2) {
+	DateTime dOne = new DateTime(d1);
+	DateTime dTwo = new DateTime(d2);
+	Period interim = new Period(dOne, dTwo);
+
+	return interim.getMinutes();
     }
-  }
-  
-  public int numMinutesDifference(Date d1, Date d2) {
-    DateTime dOne = new DateTime(d1);
-    DateTime dTwo = new DateTime(d2);
-    Period interim = new Period(dOne, dTwo);
-    
-    return interim.getMinutes();
-  }
-  
-  public int numSecondsDifference(Date d1, Date d2) {
-      DateTime dOne = new DateTime(d1);
-      DateTime dTwo = new DateTime(d2);
-      Period interim = new Period(dOne, dTwo);
-      
-      return interim.getSeconds();
-  }
+
+    public int numSecondsDifference(Date d1, Date d2) {
+	DateTime dOne = new DateTime(d1);
+	DateTime dTwo = new DateTime(d2);
+	Period interim = new Period(dOne, dTwo);
+
+	return interim.getSeconds();
+    }
 }
