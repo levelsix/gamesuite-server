cc899cf3f28cf64837db6cebf814c5edd9e1b832
Arturo Wu Zhou <aweezy90@gmail.com>
2013-07-10 18:47:34 -0700

    constructing a query instead of relying on sql

--- pictures/pictures-data-model/src/main/java/com/lvl6/pictures/dao/GameHistoryDao.java
+++ pictures/pictures-data-model/src/main/java/com/lvl6/pictures/dao/GameHistoryDao.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.lvl6.pictures.po.GameHistory;
 
@@ -18,7 +19,13 @@ public interface GameHistoryDao extends JpaRepository<GameHistory, String> {
     public List<GameHistory> findByEndTimeIsNotNullAndPlayerOneIdOrPlayerTwoId(
 	    String playerOneId, String playerTwoId);
 
-    //find recent completed games
+    //find recent completed games (not sql but hql)
+    @Query("select " +
+    		"gh " +
+	   "from GameHistory gh " +
+	   "where gh.endTime != null " +
+	    	"and gh.endTime > ?1 " +
+	    	"and (gh.playerOneId = ?2 or gh.playerTwoId = ?3)")
     public List<GameHistory> findByEndTimeGreaterThanAndPlayerOneIdOrPlayerTwoIdOrderByEndTimeDesc(
 	    Date endTime, String playerOneId, String playerTwoId);
 
